27efc873bc0617da14de95278e4c3aadf9b16731
JudahDoupe <judahdoupe@mac-pro.campus.tayloru.edu>
2014-11-21 19:57:23 -0500

    created a pretty boing script about camera stuff
    and I created God

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour 
+{
+	public float sideMoveArea = 0.95f;
+	void Update () 
+	{
+		if (Input.mousePosition.x > Screen.width * sideMoveArea)
+			Debug.Log("Hey there");
+	}
+}
